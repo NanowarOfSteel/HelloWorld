1eaa96697f2bc505a0fb0858c4c3f28a7378410c
Christian Baumann <christian.baumann@maibornwolff.de>
2024-12-04 11:42:29 +0100

    Revert "Refactor HelloWorldMainLauncherClass and Add JUnit tests"
    This reverts commit c8960ca68cc373a4321be747c41b775cc82f1105.

--- src/main/java/it/nanowar/ofsteel/helloworld/HelloWorldMainLauncherClass.java
+++ src/main/java/it/nanowar/ofsteel/helloworld/HelloWorldMainLauncherClass.java
@@ -1,54 +1,48 @@
 package it.nanowar.ofsteel.helloworld;
 
 public class HelloWorldMainLauncherClass {
-
-	private Integer foo;
-
+	
 	public HelloWorldMainLauncherClass(int foo) {
-		this.foo = foo;
+		
+		super();
+		this.foo=foo;
 	}
-
-	public int getFoo() {
-		return foo;
-	}
-
+	
+	private Integer foo=0;
+	
 	public void songRefrain() {
-		printHelloWorldMultipleTimes(foo);
-		printHelloWorldMultipleTimesWhile(foo);
-	}
-
-	private void printHelloWorldMultipleTimes(int times) {
-		for (int i = 0; i < times; i++) {
+		
+		for (int i=0; i<foo; i++) {
 			System.out.println("Hello World!");
+			
 		}
-	}
-
-	private void printHelloWorldMultipleTimesWhile(int times) {
-		int pippo = 0;
-		while (pippo < times) {
+		
+		int pippo=0;
+		while (pippo<foo) {
 			System.out.println("Hello World!");
 			pippo++;
 		}
 	}
-
-	public static void main(String[] args) {
+	
+	public static void main (String [] args) {
 		System.out.println("Hello World Programmer Start");
 		HelloWorldMainLauncherClass tizio = new HelloWorldMainLauncherClass(2);
 		tizio.songRefrain();
 		System.out.println("Program Finished!");
 		tizio.falseMethod();
 	}
-
-	/**
+	
+	/*
 	 * If I may introduce a bug, the JVM will manage it for me
 	 * ensuring both security and portability
 	 * Then I'll write my code once, and run it everywhere!
 	 * With Static and Strong Typing, will let my programs be type safe!
 	 */
+	
 	public void falseMethod() {
-		boolean metal = false;
-		if (metal == false) {
-			String joeyDeCaio = null;
+		boolean metal=false;
+		if(metal==false) {
+			String joeyDeCaio=null;
 			joeyDeCaio.length();
 		}
 	}
